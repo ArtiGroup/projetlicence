41aab8422acc8f381ee155a022563a3af72e72e9
ArtiGroup <cm.arti.group@gmail.com>
2022-07-20 20:50:06 +0100

    Adding the projects folder

---
+++ src/main/java/com/artigroup/projetlicence/ProjetlicenceApplication.java
@@ -0,0 +1,13 @@
+package com.artigroup.projetlicence;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetlicenceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetlicenceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/artigroup/projetlicence/ProjetlicenceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.artigroup.projetlicence;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetlicenceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
